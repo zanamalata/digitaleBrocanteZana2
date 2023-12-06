6e926ca2f4284755acb4caba615b0ec1899a820e
janoujan <janou@janou.pro>
2023-12-06 11:20:52 +0100

    fix for build

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { privateProcedure, publicProcedure, router } from "./trpc";
+import { privateProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "../get-payload";
 import { stripe } from "../lib/stripe";
