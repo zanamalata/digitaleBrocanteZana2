9b384d96320974f9be6a565afdba0bf7d7691250
janoujan <janou@janou.pro>
2023-12-28 12:42:09 +0100

    erase all medias

--- next.config.js
+++ next.config.js
@@ -4,13 +4,12 @@ const nextConfig = {
   // TODO change add domain to remotePattern for where the images come from
   images:{
     remotePatterns: [{
-      hostname: "localhost",
       protocol: "http",
-      port: "3000"
+      hostname: "localhost",
     },
   {
-    hostname: "digitalebrocantezana2-production.up.railway.app",
     protocol: "https",
+    hostname: "digitalebrocantezana2-production.up.railway.app",
   }],
   },
 }
