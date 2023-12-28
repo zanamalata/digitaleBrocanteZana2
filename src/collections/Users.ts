b71f8595ba202a30d4321577f7972527b6405c4c
janoujan <janou@janou.pro>
2023-12-28 19:23:44 +0100

    revert user collection

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,269 +1,341 @@
 
-import { PrimaryActionEmailHtml } from '../components/emails/PrimaryActionEmail'
-import { CollectionConfig, FieldAccess } from 'payload/types'
+// import { PrimaryActionEmailHtml } from '../components/emails/PrimaryActionEmail'
+// import { CollectionConfig, FieldAccess } from 'payload/types'
 
 
-const adminsAndSeller: FieldAccess = ({ req: { user } }) => {
-    if (user.role === 'admin') return true
-    else if (user.role === 'seller') return true
-    return false
-}
+// const adminsAndSeller: FieldAccess = ({ req: { user } }) => {
+//     if (user.role === 'admin') return true
+//     else if (user.role === 'seller') return true
+//     return false
+// }
 
-// TODO change domain name in resend when got own domain
-export const Users: CollectionConfig = {
-    slug: 'users',
-    auth: {
-        verify: {
-            generateEmailHTML: ({ token }) => {
-                return PrimaryActionEmailHtml({
-                    actionLabel: 'Verify your account',
-                    buttonText: 'verify account',
-                    href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`,
-                })
-            },
-        },
-    },
-    access: {
-        read: adminsAndSeller,
-        create: () => true,
-        update: ({ req}) => req.user.role === 'admin',
-        delete: ({ req}) => req.user.role === 'admin',
-    },
-    upload: true,
-    admin: {
-        hidden: ({ user }) => user.role !== 'admin',
-        defaultColumns: ['id'],
-    },
-    fields: [
-        {
-            name: 'username',
-            label: "Nom d'utilisateur",
-            type: 'text',
-            access: {
-                create: () => true,
-                read: () => true,
-                update: () => true,
-            },
-            admin: {
-                description: "Nom d'utilisateur: le nom que verrons les autres utilisateurs de la plateforme",
-            }
-        },
-        {
-            name: 'seller_name',
-            label: 'Nom du vendeur ou de la boutique',
-            type: 'text',
-            access: {
-                create: adminsAndSeller,
-                read: adminsAndSeller,
-                update: adminsAndSeller,
-            },
-            admin: {
-                condition: () => true,
-                description: 'Le nom du vendeur ou de la boutique',
-            },
-            required: false,
-        },
-        {
-            name: 'seller_activity',
-            label: 'activité du vendeur',
-            type: 'text',
-            required: false,
-            access: {
-                create: adminsAndSeller,
-                read: adminsAndSeller,
-                update: adminsAndSeller,
-            },
-            admin: {
-                condition: () => true,
-                description: 'activité du vendeur ou de la boutique',
-                placeholder: 'brocanteur, artisant, designer, artiste, etc...',
-            },
-        },
-        {
-            name: 'seller_description',
-            label: 'Description du vendeur ou de la boutique',
-            type: 'textarea',
-            required: false,
-            access: {
-                create: adminsAndSeller,
-                read: adminsAndSeller,
-                update: adminsAndSeller,
-            },
-            admin: {
-                condition: () => true,
-                description:
-                    'Pour embellir votre boutique donner une description de votre boutique',
-                placeholder:
-                    'description de votre boutique ou de vos articles, par exemple : je suis brocanteur spécialisé dans le style brutaliste, mes articles sont sélectionnés et trier.....',
-            },
-        },
-        // {
-        //   name: "product_files",
-        //   label: "Product files",
-        //   admin: {
-        //     condition: () => false,
-        //   },
-        //   type: "relationship",
-        //   relationTo: "product_files",
-        //   hasMany: true,
-        // },
-        {
-            name: 'role',
-            required: true,
-            defaultValue: 'user',
-            admin: {
-                condition: () => true,
-            },
-            access: {
-                read: () => true,
-                create: ({ req}) => req.user.role === 'admin',
-                update: ({ req}) => req.user.role === 'admin',
-            },
-            type: 'select',
-            options: [
-                { label: 'Admin', value: 'admin' },
-                { label: 'User', value: 'user' },
-                { label: 'Seller', value: 'seller' },
-            ],
-        },
-        {
-            name: 'products',
-            label: 'Products',
-            admin: {
-                condition: () => false,
-            },
-            type: 'relationship',
-            relationTo: 'products',
-            hasMany: true,
-        },
-        {
-            name: 'photo',
-            type: 'upload',
-            label: 'Ajouter une photo',
-            relationTo: 'media',
-            required: false,
+// // TODO change domain name in resend when got own domain
+// export const Users: CollectionConfig = {
+//     slug: 'users',
+//     auth: {
+//         verify: {
+//             generateEmailHTML: ({ token }) => {
+//                 return PrimaryActionEmailHtml({
+//                     actionLabel: 'Verify your account',
+//                     buttonText: 'verify account',
+//                     href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`,
+//                 })
+//             },
+//         },
+//     },
+//     access: {
+//         read: adminsAndSeller,
+//         create: () => true,
+//         update: ({ req}) => req.user.role === 'admin',
+//         delete: ({ req}) => req.user.role === 'admin',
+//     },
+//     upload: true,
+//     admin: {
+//         hidden: ({ user }) => user.role !== 'admin',
+//         defaultColumns: ['id'],
+//     },
+//     fields: [
+//         {
+//             name: 'username',
+//             label: "Nom d'utilisateur",
+//             type: 'text',
+//             access: {
+//                 create: () => true,
+//                 read: () => true,
+//                 update: () => true,
+//             },
+//             admin: {
+//                 description: "Nom d'utilisateur: le nom que verrons les autres utilisateurs de la plateforme",
+//             }
+//         },
+//         {
+//             name: 'seller_name',
+//             label: 'Nom du vendeur ou de la boutique',
+//             type: 'text',
+//             access: {
+//                 create: adminsAndSeller,
+//                 read: adminsAndSeller,
+//                 update: adminsAndSeller,
+//             },
+//             admin: {
+//                 condition: () => true,
+//                 description: 'Le nom du vendeur ou de la boutique',
+//             },
+//             required: false,
+//         },
+//         {
+//             name: 'seller_activity',
+//             label: 'activité du vendeur',
+//             type: 'text',
+//             required: false,
+//             access: {
+//                 create: adminsAndSeller,
+//                 read: adminsAndSeller,
+//                 update: adminsAndSeller,
+//             },
+//             admin: {
+//                 condition: () => true,
+//                 description: 'activité du vendeur ou de la boutique',
+//                 placeholder: 'brocanteur, artisant, designer, artiste, etc...',
+//             },
+//         },
+//         {
+//             name: 'seller_description',
+//             label: 'Description du vendeur ou de la boutique',
+//             type: 'textarea',
+//             required: false,
+//             access: {
+//                 create: adminsAndSeller,
+//                 read: adminsAndSeller,
+//                 update: adminsAndSeller,
+//             },
+//             admin: {
+//                 condition: () => true,
+//                 description:
+//                     'Pour embellir votre boutique donner une description de votre boutique',
+//                 placeholder:
+//                     'description de votre boutique ou de vos articles, par exemple : je suis brocanteur spécialisé dans le style brutaliste, mes articles sont sélectionnés et trier.....',
+//             },
+//         },
+//         // {
+//         //   name: "product_files",
+//         //   label: "Product files",
+//         //   admin: {
+//         //     condition: () => false,
+//         //   },
+//         //   type: "relationship",
+//         //   relationTo: "product_files",
+//         //   hasMany: true,
+//         // },
+//         {
+//             name: 'role',
+//             required: true,
+//             defaultValue: 'user',
+//             admin: {
+//                 condition: () => true,
+//             },
+//             access: {
+//                 read: () => true,
+//                 create: ({ req}) => req.user.role === 'admin',
+//                 update: ({ req}) => req.user.role === 'admin',
+//             },
+//             type: 'select',
+//             options: [
+//                 { label: 'Admin', value: 'admin' },
+//                 { label: 'User', value: 'user' },
+//                 { label: 'Seller', value: 'seller' },
+//             ],
+//         },
+//         {
+//             name: 'products',
+//             label: 'Products',
+//             admin: {
+//                 condition: () => false,
+//             },
+//             type: 'relationship',
+//             relationTo: 'products',
+//             hasMany: true,
+//         },
+//         {
+//             name: 'photo',
+//             type: 'upload',
+//             label: 'Ajouter une photo',
+//             relationTo: 'media',
+//             required: false,
             
-            access: {
-                create: adminsAndSeller,
-                read: adminsAndSeller,
-                update: adminsAndSeller,
-            },
-            admin: {
-                description:
-                'vendeurs vous pouvez rajouté une photo pour illustrer votre boutique',
-            },
-        },
-        {
-            name: 'banner',
-            type: 'upload',
-            label: 'Ajouter une bannière',
-            relationTo: 'media',
-            required: false,
-            access: {
-                create: adminsAndSeller,
-                read: adminsAndSeller,
-                update: adminsAndSeller,
-            },
-            admin: {
-                description:
-                    'vendeurs vous pouvez rajouté une bannière pour illustrer votre boutique',
-            },
-        },
-        {
-            name: 'firstname',
-            label: "Votre prénom",
-            type: 'text',
-            access: {
-                create: () => true,
-                read: () => true,
-                update: () => true,
-            },
-            admin: {
-                placeholder: 'Prénom',
-            }
-        },
-        {
-            name: 'lastname',
-            label: "Votre nom",
-            type: 'text',
-            access: {
-                create: () => true,
-                read: () => true,
-                update: () => true,
-            },
-            admin: {
-                placeholder: 'Nom',
-            }
-        },
-        // TODO changement d'adresse
-        {
-            name: 'adress1',
-            label: 'Adresse',
-            type: 'text',
-            required: false,
-            access: {
-                create: () => true,
-                read: () => true,
-                update: () => true,
-            },
-            admin: {
-                condition: () => true,
-            },
-        },
-        {
-            name: 'adress2',
-            label: "Complément d'adresse",
-            type: 'text',
-            required: false,
-            access: {
-                create: () => true,
-                read: () => true,
-                update: () => true,
-            },
-            admin: {
-                condition: () => true,
-            },
-        },
-        {
-            name: 'city',
-            label: 'Ville',
-            type: 'text',
-            required: false,
-            admin: {
-                condition: () => true,
-            },
-        },
-        {
-            // TODO add validate only number if locale is fr
-            name: 'postcode',
-            label: 'Code postal',
-            type: 'number',
-            required: false,
-            admin: {
-                condition: () => true,
-            },
-        },
-        // TODO proper localize and add flag
-        {
-            name: 'country',
-            label: 'Pays',
-            type: 'select',
-            hasMany: false,
-            localized: true,
-            options: [
-                {
-                    label: 'France',
-                    value: 'france',
-                },
-                {
-                    label: 'Germany',
-                    value: 'germany',
-                },
-                {
-                    label: 'Spain',
-                    value: 'spain',
-                },
-            ],
-        },
-    ],
+//             access: {
+//                 create: adminsAndSeller,
+//                 read: adminsAndSeller,
+//                 update: adminsAndSeller,
+//             },
+//             admin: {
+//                 description:
+//                 'vendeurs vous pouvez rajouté une photo pour illustrer votre boutique',
+//             },
+//         },
+//         {
+//             name: 'banner',
+//             type: 'upload',
+//             label: 'Ajouter une bannière',
+//             relationTo: 'media',
+//             required: false,
+//             access: {
+//                 create: adminsAndSeller,
+//                 read: adminsAndSeller,
+//                 update: adminsAndSeller,
+//             },
+//             admin: {
+//                 description:
+//                     'vendeurs vous pouvez rajouté une bannière pour illustrer votre boutique',
+//             },
+//         },
+//         {
+//             name: 'firstname',
+//             label: "Votre prénom",
+//             type: 'text',
+//             access: {
+//                 create: () => true,
+//                 read: () => true,
+//                 update: () => true,
+//             },
+//             admin: {
+//                 placeholder: 'Prénom',
+//             }
+//         },
+//         {
+//             name: 'lastname',
+//             label: "Votre nom",
+//             type: 'text',
+//             access: {
+//                 create: () => true,
+//                 read: () => true,
+//                 update: () => true,
+//             },
+//             admin: {
+//                 placeholder: 'Nom',
+//             }
+//         },
+//         // TODO changement d'adresse
+//         {
+//             name: 'adress1',
+//             label: 'Adresse',
+//             type: 'text',
+//             required: false,
+//             access: {
+//                 create: () => true,
+//                 read: () => true,
+//                 update: () => true,
+//             },
+//             admin: {
+//                 condition: () => true,
+//             },
+//         },
+//         {
+//             name: 'adress2',
+//             label: "Complément d'adresse",
+//             type: 'text',
+//             required: false,
+//             access: {
+//                 create: () => true,
+//                 read: () => true,
+//                 update: () => true,
+//             },
+//             admin: {
+//                 condition: () => true,
+//             },
+//         },
+//         {
+//             name: 'city',
+//             label: 'Ville',
+//             type: 'text',
+//             required: false,
+//             admin: {
+//                 condition: () => true,
+//             },
+//         },
+//         {
+//             // TODO add validate only number if locale is fr
+//             name: 'postcode',
+//             label: 'Code postal',
+//             type: 'number',
+//             required: false,
+//             admin: {
+//                 condition: () => true,
+//             },
+//         },
+//         // TODO proper localize and add flag
+//         {
+//             name: 'country',
+//             label: 'Pays',
+//             type: 'select',
+//             hasMany: false,
+//             localized: true,
+//             options: [
+//                 {
+//                     label: 'France',
+//                     value: 'france',
+//                 },
+//                 {
+//                     label: 'Germany',
+//                     value: 'germany',
+//                 },
+//                 {
+//                     label: 'Spain',
+//                     value: 'spain',
+//                 },
+//             ],
+//         },
+//     ],
+// }
+
+import { PrimaryActionEmailHtml } from '../components/emails/PrimaryActionEmail'
+import { Access, CollectionConfig } from 'payload/types'
+
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user.role === 'admin') return true
+
+  return {
+    id: {
+      equals: user.id,
+    },
+  }
 }
+
+export const Users: CollectionConfig = {
+  slug: 'users',
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return PrimaryActionEmailHtml({
+          actionLabel: "verify your account",
+          buttonText: "Verify Account",
+          href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`
+        })
+      },
+    },
+  },
+  access: {
+    read: adminsAndUser,
+    create: () => true,
+    update: ({ req }) => req.user.role === 'admin',
+    delete: ({ req }) => req.user.role === 'admin',
+  },
+  admin: {
+    hidden: ({ user }) => user.role !== 'admin',
+    defaultColumns: ['id'],
+  },
+  fields: [
+    {
+      name: 'products',
+      label: 'Products',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'products',
+      hasMany: true,
+    },
+    // {
+    //   name: 'product_files',
+    //   label: 'Product files',
+    //   admin: {
+    //     condition: () => false,
+    //   },
+    //   type: 'relationship',
+    //   relationTo: 'product_files',
+    //   hasMany: true,
+    // },
+    {
+      name: 'role',
+      defaultValue: 'user',
+      required: true,
+
+      type: 'select',
+      options: [
+        { label: 'Admin', value: 'admin' },
+        { label: 'User', value: 'user' },
+      ],
+    },
+  ],
+}
+
