b1477be51f169f58a081e341e57d7163d5d55379
janoujan <janou@janou.pro>
2023-12-29 11:55:16 +0100

    add seller descriptions

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -269,7 +269,7 @@
 // }
 
 import { PrimaryActionEmailHtml } from '../components/emails/PrimaryActionEmail'
-import { Access, CollectionConfig } from 'payload/types'
+import { Access, CollectionConfig, FieldAccess } from 'payload/types'
 
 const adminsAndUser: Access = ({ req: { user } }) => {
   if (user.role === 'admin') return true
@@ -280,6 +280,13 @@ const adminsAndUser: Access = ({ req: { user } }) => {
     },
   }
 }
+
+const adminsAndSeller: FieldAccess = ({ req: { user } }) => {
+  if (user.role === 'admin') return true
+  else if (user.role === 'seller') return true
+  else return false
+}
+
 
 export const Users: CollectionConfig = {
   slug: 'users',
@@ -337,6 +344,68 @@ export const Users: CollectionConfig = {
         { label: 'Seller', value: 'seller'},
       ],
     },
+    {
+                  name: 'username',
+                  label: "Nom d'utilisateur",
+                  type: 'text',
+                  access: {
+                      create: () => true,
+                      read: () => true,
+                      update: () => true,
+                  },
+                  admin: {
+                      description: "Nom d'utilisateur: le nom que verrons les autres utilisateurs de la plateforme",
+                  }
+              },
+              {
+                  name: 'seller_name',
+                  label: 'Nom du vendeur ou de la boutique',
+                  type: 'text',
+                  access: {
+                      create: adminsAndSeller,
+                      read: adminsAndSeller,
+                      update: adminsAndSeller,
+                  },
+                  admin: {
+                      condition: () => true,
+                      description: 'Le nom du vendeur ou de la boutique',
+                  },
+                  required: false,
+              },
+              {
+                  name: 'seller_activity',
+                  label: 'activité du vendeur',
+                  type: 'text',
+                  required: false,
+                  access: {
+                      create: adminsAndSeller,
+                      read: adminsAndSeller,
+                      update: adminsAndSeller,
+                  },
+                  admin: {
+                      condition: () => true,
+                      description: 'activité du vendeur ou de la boutique',
+                      placeholder: 'brocanteur, artisant, designer, artiste, etc...',
+                  },
+              },
+              {
+                  name: 'seller_description',
+                  label: 'Description du vendeur ou de la boutique',
+                  type: 'textarea',
+                  required: false,
+                  access: {
+                      create: adminsAndSeller,
+                      read: adminsAndSeller,
+                      update: adminsAndSeller,
+                  },
+                  admin: {
+                      condition: () => true,
+                      description:
+                          'Pour embellir votre boutique donner une description de votre boutique',
+                      placeholder:
+                          'description de votre boutique ou de vos articles, par exemple : je suis brocanteur spécialisé dans le style brutaliste, mes articles sont sélectionnés et trier.....',
+                  },
+              },
   ],
 }
 
