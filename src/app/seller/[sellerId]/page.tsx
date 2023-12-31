658671ef41720f4f9e3cb60f7b0d35bbb5f36cb6
janoujan <janou@janou.pro>
2023-12-31 17:19:32 +0100

    wip seller page

--- src/app/seller/[sellerId]/page.tsx
+++ src/app/seller/[sellerId]/page.tsx
@@ -1,4 +1,6 @@
+import { Products } from '@/collections/Products/products'
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import ProductReel from '@/components/ProductReel'
 import { getPayloadClient } from '@/get-payload'
 import Image from 'next/image'
 
@@ -33,22 +35,30 @@ const Page = async ({ params }: PageProps) => {
 
     const [seller] = sellers
 
-    // let imageSrc = '' as string
-    // if (
-    //     typeof seller.banner === 'object' &&
-    //     seller.banner?.hasOwnProperty('url') &&
-    //     seller.banner.url !== undefined
-    // ) {
-    //     imageSrc = seller.banner.url as string
-    // }
+    const { docs: products } = await payload.find({
+        collection: 'products',
+        where: {
+            user: {
+                equals: seller.id,
+            },
+            approvedForSale: {
+                equals: 'approved'
+            }
+        },
+    })
+console.log('products:::', products)
+const [product] = products
+
+console.log('product::::', product)
+
 
     return (
         <MaxWidthWrapper>
             <div className="relative top-10">
                 <div className="relative h-72 mt-20 bg-slate-200 rounded-t-md border border-slate-300">
-                    {seller.banner && typeof seller.banner === 'object' ? (
+                    {seller.banner && typeof seller.banner === 'string' ? (
                         <Image
-                            src={(seller.banner as { url: string }).url}
+                            src={seller.banner}
                             alt="shop banner"
                             className="rounded-t-md"
                             fill
@@ -66,28 +76,43 @@ const Page = async ({ params }: PageProps) => {
                         />
                     )}
                 </div>
-                <div className="relative rounded-full border border-slate-300 h-40 w-40 -top-6 ml-6 ">
-                    {seller.photo && typeof seller.photo === 'object' ? (
-                        <Image
-                            src={(seller.photo as { url: string }).url}
-                            alt="shop banner"
-                            className="rounded-full"
-                            fill
-                            placeholder="blur"
-                            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
-                        />
-                    ) : (
-                        <Image
-                            src={'/blank-avatar-photo.webp'}
-                            alt="shop banner"
-                            className="rounded-full"
-                            fill
-                            placeholder="blur"
-                            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
-                        />
-                    )}
+                <div className="w-fit pr-6 flex flex-col items-center">
+                    <div className="relative rounded-full border border-slate-300 h-40 w-40 -top-6 ml-6 ">
+                        {seller.photo && typeof seller.photo === 'string' ? (
+                            <Image
+                                src={seller.photo}
+                                alt="shop banner"
+                                className="rounded-full"
+                                fill
+                                placeholder="blur"
+                                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
+                            />
+                        ) : (
+                            <Image
+                                src={'/blank-avatar-photo.webp'}
+                                alt="shop banner"
+                                className="rounded-full"
+                                fill
+                                placeholder="blur"
+                                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
+                            />
+                        )}
+                    </div>
+                    <p className="relative w-fit ml-6 -mt-4 px-4 py-1 text-2xl font-medium rounded-md border border-slate-200 bg-slate-100">
+                        {seller.seller_activity}
+                    </p>
                 </div>
             </div>
+            <div className="relative flex flex-col -top-24 justify-center items-center">
+                <h1 className="text-6xl font-bold underline">
+                    {seller.seller_name}
+                </h1>
+                <p className="mt-12 text-xl">{seller.seller_description}</p>
+            </div>
+            <ProductReel
+                title={`Produits de ${seller.seller_name}`}
+                query={{ sort: 'asc' }}
+            />
         </MaxWidthWrapper>
     )
 }
