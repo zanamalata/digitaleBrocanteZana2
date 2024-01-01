ea66177718c19fa8fdc6cd59914270d4dc87b8a4
janoujan <janou@janou.pro>
2024-01-01 11:10:04 +0100

    changed path in nextjs config

--- next.config.js
+++ next.config.js
@@ -17,10 +17,11 @@ const nextConfig = {
   async headers() {   
     return [{     
       // matching all API routes   TODO change cors when in production   
+     
       // source: "/api/:path*",     
       // headers: [         
       //   { key: "Access-Control-Allow-Credentials", value: "true" },         
-      //   { key: "Access-Control-Allow-Origin", value: "*" },         
+      //   { key: "Access-Control-Allow-Origin", value: "" },         
       //   { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },         
       //   { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },       
       // ] 
@@ -28,7 +29,7 @@ const nextConfig = {
       source: "/:path*",     
       headers: [         
         { key: "Access-Control-Allow-Credentials", value: "true" },         
-        { key: "Access-Control-Allow-Origin", value: "*" },         
+        { key: "Access-Control-Allow-Origin", value: "http://localhost:3000, https://digitalebrocantezana2-production.up.railway.app" },         
         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },         
         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },       
       ] 
