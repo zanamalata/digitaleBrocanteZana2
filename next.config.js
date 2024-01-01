693ae96dd4ba03fc4b534dcd29223880904bb5ef
janoujan <janou@janou.pro>
2024-01-01 12:29:39 +0100

    changed path in nextjs config

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
       source: "/:path*",     
       headers: [         
         { key: "Access-Control-Allow-Credentials", value: "true" },         
-        { key: "Access-Control-Allow-Origin", value: "http://localhost:3000, https://digitalebrocantezana2-production.up.railway.app" },         
+        { key: "Access-Control-Allow-Origin", value: ["http://localhost:3000", "https://digitalebrocantezana2-production.up.railway.app"] },         
         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },         
         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },       
       ] 
